99cfe5afbec9fdd713ee63da0b0e6d95ce19a2bb
benRenard <benjamin.renard@inrae.fr>
2025-04-15 18:10:01 +0200

    advanced 'EauFeu' sonification

--- src/eauFeu/sonification.R
+++ src/eauFeu/sonification.R
@@ -2,20 +2,53 @@ library(musicXML)
 library(dplyr)
 
 load('extents_model5.RData')
-gamme=rev(c('E2','F2','G2',
-            'A2','C3','D3','F3',
-            'A3','C4','D4','F4','G4',
-            'A4','B4','C5','E5','F5','A5'))
-out=list()
-for(var in c('SWI','FWI','Q')){
-  mask=dat$var==var
-  x=(dat$extent[mask]/100)^0.5
-  vol=loudnessMapping(x,lMin=54)
-  p=pitchMapping(x,gamme)
-  out[[var]]=getNotes(pitches=p,loudnesses=vol) %>% tieNotes() %>%
-    getMeasures(beats=6,beatType=8)
-}
+
+all=list();k=0
+
+# SWI ----
+gamme=(c('F1','G2','A2','A2','B2','C3','D3','E3','G3','C4','E4','G4'))
+var='SWI'
+mask=dat$var==var
+x=(dat$extent[mask]/100)^1
+# x[diff(x)<0]=0
+vol=loudnessMapping(x,lMin=40,lMax=110)
+for(i in 2:length(x)){if(x[i]==0)x[i]=x[i-1]};plot(x[1000:1120],type='b')
+p=pitchMapping(x,gamme)
+k=k+1;all[[k]]=getNotes(pitches=p,loudnesses=vol) %>% tieNotes() %>%
+  getMeasures(beats=6,beatType=8)
+
+# FWI ----
+gamme=rev(c('A3','C4','D4','E4','G4',
+            'A4','B4','C5','E5'))
+var='FWI'
+mask=dat$var==var
+x=(dat$extent[mask]/100)^1
+vol=loudnessMapping(x,lMin=0,lMax=110)
+for(i in 2:length(x)){if(x[i]==0)x[i]=x[i-1]};plot(x[1000:1120],type='l')
+p=pitchMapping(x,gamme)
+k=k+1;all[[k]]=getNotes(pitches=p,loudnesses=vol) %>% tieNotes() %>%
+  getMeasures(beats=6,beatType=8)
+
+# Q -----
+var='Q'
+mask=dat$var==var
+x=(dat$extent[mask]/100)^0.5
+plot(x,type='l')
+# Q - cymbals
+# gamme=rev(c('Db3','A3','G3',
+#             'F3','B3','B3','B3','B3'))
+gamme=rev(c('F3','B3','B3','B3','B3'))
+vol=loudnessMapping(x,lMin=60,lMax=120)
+p=pitchMapping(x,gamme)
+k=k+1;all[[k]]=getNotes(pitches=p,loudnesses=vol) %>% #tieNotes() %>%
+  getMeasures(beats=6,beatType=8)
+# Q - Toms
+gamme=rev(c('B1','C2','F2','G2','A2','B2'))
+vol=loudnessMapping(x,lMin=64,lMax=110)
+p=pitchMapping(x,gamme)
+k=k+1;all[[k]]=getNotes(pitches=p,loudnesses=vol) %>% tieNotes() %>%
+  getMeasures(beats=6,beatType=8)
 
 
-writeMXL(score(out),'score.xml')
+writeMXL(score(all),'score.xml')
 

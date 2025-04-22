a5f0d8a6c377a87695568c648699525160f2b32d
benRenard <benjamin.renard@inrae.fr>
2025-04-22 08:54:26 +0200

    advanced 'EauFeu' sonification

--- src/eauFeu/sonification.R
+++ src/eauFeu/sonification.R
@@ -5,89 +5,88 @@ load('extents_model5.RData')
 
 all=list();k=0
 
-chords=list(c('F2','F3','G3','C4','G4'),
-            c('G2','Eb3','Bb3','D4','E4'),
-            c('A2','E3','G3','C4','G4'),
-            c('Bb2','F3','Bb3','C4','F4'),
-            c('C3','G4','C4','D4','G4'),
-            c('D3','A3','C4','G4','A4'))
+chords=list(
+  c('E3','B3','D4','E4','G4','B4'),
+  c('Bb3','D4','Bb4','D5','E5','F5'),
+  c('B3','Gb4','G4','B4','D5','E5'),
+  c('C4','G5','C5','D5','G5','B5'),
+  c('E4','B4','Eb5','E5','Gb5','B5'))
 
-# SWI ----
-vMax=120
+chords=list(
+  c('E3','B3','E4','G4','D5','E5'),
+  c('F3','C4','F4','G4','C5','G5'),
+  c('A3','E4','G4','C5','G5','A5'),
+  c('B3','D4','A4','D5','F5','A5'),
+  c('C4','G4','C5','D5','G5','B5'),
+  # c('E4','B4','Eb5','E5','Gb5','B5')
+  c('E3','E4','B4','F5','A5','B5'),
+  c('E3','E4','C5','E5','G5','B5'),
+  c('E3','E4','B4','E5','G5','B5'))
+
+# First pass to get chords from SWI----
 what='SWI'
-DF=dat %>% filter(var==what)
-x=(dat$extent[mask]/100)^1
+DF=dat %>% filter(var==what) %>% mutate(pitch=extent^0.5)
+plot(DF$tim,DF$pitch,type='l')
 years=unique(DF$year)
+iChords=rep(NA,length(years))
+for(i in 1:length(years)){
+  dy=DF %>% filter(year==years[i])
+  x=dy$pitch/max(DF$pitch)
+  maxi=1-max(x)
+  iChords[i]=max(ceiling(maxi*length(chords)),1)
+}
+plot(iChords,type='b')
+
+# Q ----
+vMin=80;vMax=110
+what='Q'
+DF=dat %>% filter(var==what) %>% mutate(pitch=extent^0.75)
+plot(DF$tim,DF$pitch,type='l')
 notes=list()
 for(i in 1:length(years)){
   dy=DF %>% filter(year==years[i])
-  x=dy$extent/100
-  maxi=max(x)
-  k=max(ceiling(maxi*length(chords)),1)
-  vol=x*vMax
-  p=pitchMapping(x,rev(chords[[k]]))
+  x=dy$pitch/max(DF$pitch)
+  vol=vMin+x*(vMax-vMin)
+  p=pitchMapping(c(x,0.2),rev(chords[[iChords[i]]]))[1:12]
   notes=c(notes,getNotes(pitches=p,loudnesses=vol) %>% tieNotes())
 }
-foo=getMeasures(notes,beats=6,beatType=8)
-writeMXL(score(foo),'foo.xml')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-gamme=(c('F1','G2','A2','A2','B2','C3','D3','E3','G3','C4','E4','G4'))
-var='SWI'
-mask=dat$var==var
-x=(dat$extent[mask]/100)^1
-# x[diff(x)<0]=0
-vol=loudnessMapping(x,lMin=40,lMax=110)
-for(i in 2:length(x)){if(x[i]==0)x[i]=x[i-1]};plot(x[1000:1120],type='b')
-p=pitchMapping(x,gamme)
-k=k+1;all[[k]]=getNotes(pitches=p,loudnesses=vol) %>% tieNotes() %>%
-  getMeasures(beats=6,beatType=8)
+k=k+1;all[[k]]=getMeasures(notes %>% tieNotes(),beats=6,beatType=8)
+writeMXL(score(all),'Q.xml')
 
 # FWI ----
-gamme=rev(c('A3','C4','D4','E4','G4',
-            'A4','B4','C5','E5'))
-var='FWI'
-mask=dat$var==var
-x=(dat$extent[mask]/100)^1
-vol=loudnessMapping(x,lMin=0,lMax=110)
-for(i in 2:length(x)){if(x[i]==0)x[i]=x[i-1]};plot(x[1000:1120],type='l')
-p=pitchMapping(x,gamme)
-k=k+1;all[[k]]=getNotes(pitches=p,loudnesses=vol) %>% tieNotes() %>%
-  getMeasures(beats=6,beatType=8)
+vMin=80;vMax=110
+what='FWI'
+DF=dat %>% filter(var==what) %>% mutate(pitch=extent^0.75)
+plot(DF$tim,DF$pitch,type='l')
+notes=list()
+for(i in 1:length(years)){
+  dy=DF %>% filter(year==years[i])
+  x=dy$pitch/max(DF$pitch)
+  vol=vMin+x*(vMax-vMin)
+  p=pitchMapping(c(x,0.2),rev(chords[[iChords[i]]]))[1:12]
+  notes=c(notes,getNotes(pitches=p,loudnesses=vol) %>% tieNotes())
+}
+k=k+1;all[[k]]=getMeasures(notes %>% tieNotes(),beats=6,beatType=8)
+writeMXL(score(all),'FWI.xml')
 
-# Q -----
-var='Q'
-mask=dat$var==var
-x=(dat$extent[mask]/100)^0.5
-plot(x,type='l')
-# Q - cymbals
-# gamme=rev(c('Db3','A3','G3',
-#             'F3','B3','B3','B3','B3'))
-gamme=rev(c('F3','B3','B3','B3','B3'))
-vol=loudnessMapping(x,lMin=60,lMax=120)
-p=pitchMapping(x,gamme)
-k=k+1;all[[k]]=getNotes(pitches=p,loudnesses=vol) %>% #tieNotes() %>%
-  getMeasures(beats=6,beatType=8)
-# Q - Toms
+# SWI -----
+what='SWI'
+DF=dat %>% filter(var==what) %>% mutate(pitch=extent^0.5)
+plot(DF$tim,DF$pitch,type='l')
+
+# Toms
 gamme=rev(c('B1','C2','F2','G2','A2','B2'))
-vol=loudnessMapping(x,lMin=64,lMax=110)
-p=pitchMapping(x,gamme)
+vol=loudnessMapping(DF$pitch,lMin=64,lMax=110)
+p=pitchMapping(DF$pitch,gamme)
 k=k+1;all[[k]]=getNotes(pitches=p,loudnesses=vol) %>% tieNotes() %>%
   getMeasures(beats=6,beatType=8)
-
+# 
+# # cymbals
+# gamme=rev(c('F3','B3','B3','B3','B3'))
+# vol=loudnessMapping(x,lMin=60,lMax=120)
+# p=pitchMapping(x,gamme)
+# k=k+1;all[[k]]=getNotes(pitches=p,loudnesses=vol) %>% #tieNotes() %>%
+#   getMeasures(beats=6,beatType=8)
 
 writeMXL(score(all),'score.xml')
 
